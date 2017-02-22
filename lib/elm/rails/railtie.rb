ab2092299676b1e6c5cb4d4551f80c293c1efe4c
Valdes, Noriel <nvaldes@c21stores.com>
2017-02-22 11:54:21 -0500

    use Sprockets::Environment#append_path

--- lib/elm/rails/railtie.rb
+++ lib/elm/rails/railtie.rb
@@ -1,26 +1,28 @@
-require "rails"
-require "elm/rails/sprockets"
+require 'rails'
+require 'elm/rails/sprockets'
 
 module Elm
   module Rails
     class Railtie < ::Rails::Railtie
-      initializer "elm-rails.setup_view_helpers", group: :all do |app|
+      initializer 'elm-rails.setup_view_helpers', group: :all do
         ActiveSupport.on_load(:action_view) do
           include ::Elm::Rails::Helper
         end
       end
 
-      initializer "elm-rails.setup_engine", :group => :all do |app|
+      initializer 'elm-rails.setup_engine', group: :all do |app|
         sprockets_env = app.assets || app.config.assets
-        sprockets_env.paths += Dir[app.root.join("app", "assets", "elm")]
+        Dir[app.root.join('app', 'assets', 'elm')].each do |path|
+          sprockets_env.append_path(path)
+        end
 
         config.assets.configure do |env|
-          env.register_mime_type "text/x-elm", extensions: [".elm"]
+          env.register_mime_type 'text/x-elm', extensions: ['.elm']
 
-          if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new("3.0.0")
-            env.register_transformer "text/x-elm", "application/javascript", Elm::Rails::Sprockets
+          if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('3.0.0')
+            env.register_transformer 'text/x-elm', 'application/javascript', Elm::Rails::Sprockets
           else
-            env.register_engine ".elm", Elm::Rails::Sprockets
+            env.register_engine '.elm', Elm::Rails::Sprockets
           end
         end
       end
